4983ed25c28a9098eebf36b50579b51143239a11
danko1981 <145504669+danko1981@users.noreply.github.com>
2023-09-20 21:32:32 +0200

    Add files via upload

---
+++ WinInstallPwsh.ps1
@@ -0,0 +1,77 @@
+#####################################################################
+#INSTALL POWERSHELL 7 LTS
+#####################################################################
+#Generate dowload link for latest LTS release.
+$buildinfoUrl = 'https://aka.ms/pwsh-buildinfo-lts'
+Write-Verbose "`$buildinfoUrl`t$buildinfoUrl"
+$releaseTag = (Invoke-RestMethod -UseBasicParsing -Uri $buildinfoUrl -ErrorAction Stop -Verbose:$false).ReleaseTag
+$versionNumber = $releaseTag.trim("v")
+$architecture = "x64"
+$ZipUrl = "https://github.com/PowerShell/PowerShell/releases/download/$($releaseTag)/PowerShell-$($versionNumber)-win-$architecture.msi"
+Write-Host  $ZipUrl  
+
+#Download the PowerShell 7 installer
+Invoke-WebRequest -Uri $ZipUrl -OutFile "$env:TEMP\PowerShell-7-x64.msi"
+
+#Install PowerShell 7
+try {
+    Start-Process -Wait -FilePath "$env:TEMP\PowerShell-7-x64.msi" -ArgumentList "/qn" -PassThru  
+    Write-Host "Powershell $versionNumber installed"
+}
+catch {
+    Write-Host "Error while installing powershell $versionNumber"
+}
+
+#####################################################################
+#INSTALL Latest VCRedist
+#####################################################################
+# Define the URL for the VcRedist.x64 installer
+$vcInstallerUrl = "https://aka.ms/vs/17/release/vc_redist.x64.exe"
+
+# Download the installer
+Invoke-WebRequest -Uri $vcInstallerUrl -OutFile "$env:TEMP\vc_redist.x64.exe"
+
+#Install VcRedistr
+try {
+    Start-Process -Wait -FilePath "$env:TEMP\vc_redist.x64.exe" -ArgumentList "/install /quiet /norestart" -PassThru  
+    Write-Host "Visual C++ Redistributable successfully Installed "
+}
+catch {
+    Write-Host "Error while installing Visual C++ Redistributable"
+}
+
+#####################################################################
+#WINDOWS SERVER ROLES AND FEATURES
+#####################################################################
+
+Install-WindowsFeature -Name Web-Server -IncludeManagementTools
+Install-WindowsFeature -Name WAS
+Install-WindowsFeature -Name Web-WHC
+Install-WindowsFeature -Name NET-Framework-45-Features
+Install-WindowsFeature -Name NET-HTTP-Activation
+Install-WindowsFeature -Name NET-Framework-45-ASPNET
+Install-WindowsFeature -Name NET-WCF-TCP-Activation45
+Install-WindowsFeature -Name NET-WCF-HTTP-Activation45
+Install-WindowsFeature -Name BITS-IIS-Ext
+
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpRedirect -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationDevelopment -All
+
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASP -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET45 -All
+Enable-WindowsOptionalFeature -Online -FeatureName NetFx4Extended-ASPNET45 -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-NetFxExtensibility45 -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebSockets -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationInit -All
+
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-LoggingLibraries -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-RequestMonitor -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpTracing -All
+
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-BasicAuthentication -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WindowsAuthentication -All
+
+
+
+
+
